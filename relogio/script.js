4817dae02336c12f673bc8bd66845a2cf2d600a9
joaowildson <93096001+joaowildson@users.noreply.github.com>
2024-06-08 19:02:48 -0300

    Update script.js

--- relogio/script.js
+++ relogio/script.js
@@ -1,11 +1,19 @@
 function atualizarTempo() {
     var agora = new Date();
  
-    var horas = `${agora.getHours()}:${agora.getMinutes()}:${agora.getSeconds()}`
+    var horas = `${corrigirHora(agora.getHours())}:${corrigirHora(agora.getMinutes())}:${corrigirHora(agora.getSeconds())}`
 
     var relogio = document.querySelector(".display");
 
     relogio.textContent = horas
 }
+
+function corrigirHora(numero) {
+    if(numero < 10) {
+        numero = '0' + numero;
+    }
+    
+    return numero;
+} 
 
 setInterval(atualizarTempo, 1000);
